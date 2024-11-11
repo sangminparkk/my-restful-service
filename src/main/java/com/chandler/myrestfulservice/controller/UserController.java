f5168d8c726bf5f5fee66652fa30fc26abe54d49
chandler <parksm0529@gmail.com>
2024-11-11 22:11:01 +0900

    feat) GET /users to find users and specific user

---
+++ src/main/java/com/chandler/myrestfulservice/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.chandler.myrestfulservice.controller;
+
+import com.chandler.myrestfulservice.domain.User;
+import com.chandler.myrestfulservice.service.UserDaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final UserDaoService userDaoService;
+
+    @GetMapping("/users")
+    public List<User> usersList() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User findUser(@PathVariable Integer id) {
+        return userDaoService.findOne(id);
+    }
+
+}
+
