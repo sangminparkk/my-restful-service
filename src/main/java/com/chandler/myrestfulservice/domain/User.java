4b0bb7641f81df1e33c03e370733f0995380b43d
chandler <parksm0529@gmail.com>
2024-11-16 14:52:43 +0900

    feat) JPA configuration

--- src/main/java/com/chandler/myrestfulservice/domain/User.java
+++ src/main/java/com/chandler/myrestfulservice/domain/User.java
@@ -2,22 +2,27 @@ package com.chandler.myrestfulservice.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Schema(description = "사용자 상세 정보를 위한 도메인")
 @JsonIgnoreProperties(value = {"password", "ssn"})
 @Getter
 @Setter
 @NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "USERS")
 public class User {
 
+    @Id @GeneratedValue(strategy = IDENTITY)
     @Schema(title = "사용자 ID", description = "사용자 ID는 자동으로 생성됩니다.")
     private Integer id;
 
