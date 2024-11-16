47e8afeb345f008ddac5275eeb9afbaa1f78222b
chandler <parksm0529@gmail.com>
2024-11-16 14:54:04 +0900

    fix) Access h2-console through security configuration

--- src/main/java/com/chandler/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/com/chandler/myrestfulservice/config/SecurityConfig.java
@@ -1,15 +1,32 @@
 package com.chandler.myrestfulservice.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .anyRequest().authenticated())
+                .csrf(csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console()))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
