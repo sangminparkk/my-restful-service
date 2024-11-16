4a5ed7e2c822612330430a053519359e14413c31
chandler <parksm0529@gmail.com>
2024-11-16 16:00:05 +0900

    feat) Insert initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(joined_at, name, password, ssn) VALUES (now(), 'user1', 'test1', '1111-1111');
+insert into users(joined_at, name, password, ssn) VALUES (now(), 'user2', 'test2', '2222-2222');
+insert into users(joined_at, name, password, ssn) VALUES (now(), 'user3', 'test3', '3333-3333');
